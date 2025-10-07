5218bc063cfae237ebc9dbefece1b8efc340ad6b
irkaneprobirka <liptonhwange@gmail.com>
2025-10-07 13:52:22 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,29 @@ import React, { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "http://localhost:3000/bundle.js";
+    script.src = "http://172.16.1.69:3000/bundle.js";
     script.async = true;
+    
+    script.onload = () => {
+      console.log("ok");
+    };
+    
+    script.onerror = () => {
+      console.error("error");
+    };
+    
     document.body.appendChild(script);
+
+    return () => {
+      if (document.body.contains(script)) {
+        document.body.removeChild(script);
+      }
+    };
   }, []);
 
   return (
-      <div id="root"></div>
+    <div id="root">
+        <p>Подключение к: 172.16.1.69:3000</p>
+    </div>
   );
 }
